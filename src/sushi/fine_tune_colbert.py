acb511898b7b38546537e235e7e3f0a953ce4ee9
log4jDominion <bhardwajshashank1506@gmail.com>
2024-11-18 00:00:30 -0500

    Removed all params

--- src/sushi/fine_tune_colbert.py
+++ src/sushi/fine_tune_colbert.py
@@ -47,7 +47,7 @@ def build_qrels(queries):
 def fine_tune_colbert():
     base_url = os.getenv(Vars.RESOURCES.name)
     with Run().context(RunConfig(nranks=1, experiment=experiment_name)):
-        config = ColBERTConfig(lr=1e-05, warmup=20000, doc_maxlen=180, dim=128, attend_to_mask_tokens=False, accumsteps=1, similarity='cosine', use_ib_negatives=True)
+        config = ColBERTConfig(nranks=1, experiment="sushi_trainings")
         trainer = Trainer(triples=base_url + '/sushi/triples.jsonl', queries=base_url + '/sushi/queries.tsv',
             collection=base_url + '/sushi/collection.tsv', config=config, )
 
