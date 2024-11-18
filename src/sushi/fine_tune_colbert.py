e32936153000ecb21262a570426409224d5c7f0b
log4jDominion <bhardwajshashank1506@gmail.com>
2024-11-18 16:40:02 -0500

    Update fine_tune_colbert.py

--- src/sushi/fine_tune_colbert.py
+++ src/sushi/fine_tune_colbert.py
@@ -48,10 +48,9 @@ def fine_tune_colbert():
     base_url = os.getenv(Vars.RESOURCES.name)
     with Run().context(RunConfig(nranks=1, experiment=experiment_name)):
         config = ColBERTConfig(similarity='cosine')
-        trainer = Trainer(triples=base_url + '/msmarco/triples.train.small.tsv', config=config, )
+        trainer = Trainer(triples=base_url + '/msmarco/triples.train.small.tsv', queries=base_url + '/msmarco/queries/queries.train.tsv',
+                          collection=base_url + '/msmarco/collection.tsv', config=config, )
 
-        # queries=base_url + '/msmarco/queries/queries.train.tsv', collection=base_url + '/msmarco/collection.tsv'
-        
         trainer.train(checkpoint='colbert-ir/colbertv1.9')
         checkpoint_path = trainer.best_checkpoint_path()
 
