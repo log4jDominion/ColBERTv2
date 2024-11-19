4de7a83949a9f282a231badd6267ae15b225618a
log4jDominion <bhardwajshashank1506@gmail.com>
2024-11-18 23:35:22 -0500

    Changed file name

--- src/sushi/fine_tune_colbert.py
+++ src/sushi/fine_tune_colbert.py
@@ -48,7 +48,7 @@ def fine_tune_colbert():
     base_url = os.getenv(Vars.RESOURCES.name)
     with Run().context(RunConfig(nranks=1, experiment=experiment_name)):
         config = ColBERTConfig(similarity='cosine')
-        trainer = Trainer(triples=base_url + '/msmarco/triples.train.small.tsv', queries=base_url + '/msmarco/queries/queries.train.tsv',
+        trainer = Trainer(triples=base_url + '/msmarco/examples.json', queries=base_url + '/msmarco/queries/queries.train.tsv',
                           collection=base_url + '/msmarco/collection.tsv', config=config, )
 
         trainer.train(checkpoint='colbert-ir/colbertv1.9')
