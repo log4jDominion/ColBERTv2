bf33f5b5a2396ba7856a2766b5c7bce24eb4d9fc
log4jDominion <bhardwajshashank1506@gmail.com>
2024-12-14 19:20:24 -0500

    Training on control file

--- src/sushi/sushi_main.py
+++ src/sushi/sushi_main.py
@@ -41,14 +41,14 @@ def eval_model():
     # colbert.fine_tune_colbert()
 
     #
-    training_dataset, labels = data_util.create_complete_collection()
-    colbert.train_colbert(training_dataset, labels)
+    # training_dataset, labels = data_util.create_complete_collection()
+    # colbert.train_colbert(training_dataset, labels)
 
     for experimentSet in control_file['ExperimentSets']:
         # training_dataset, labels = data_util.create_complete_collection()
         # training_dataset, labels = data_util.create_complete_collection()
-        # training_dataset, labels = data_util.extract_label_training_dataset(experimentSet['TrainingDocuments'],search_fields)
-
+        training_dataset, labels = data_util.extract_label_training_dataset(experimentSet['TrainingDocuments'],search_fields)
+        colbert.train_colbert(training_dataset, labels)
         topics = list(experimentSet['Topics'].keys())
         print(f'No of topics: {len(topics)}')
         for j in range(len(topics)):
