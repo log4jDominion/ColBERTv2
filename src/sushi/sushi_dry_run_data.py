494ea4c2da0132d0f66c8169fef1acb091389735
log4jDominion <bhardwajshashank1506@gmail.com>
2025-01-02 11:15:29 -0800

    Update sushi_dry_run_data.py

--- src/sushi/sushi_dry_run_data.py
+++ src/sushi/sushi_dry_run_data.py
@@ -205,8 +205,10 @@ def create_trainingSet(trainingDocs):
                 ocr = title
 
         text = summary + ' ' + ocr
+        # trainingSet.append(
+        #     {'docno': file, 'folder': folder, 'box': box, 'title': title, 'ocr': text, 'folderlabel': label})
         trainingSet.append(
-            {'docno': file, 'folder': folder, 'box': box, 'title': title, 'ocr': text, 'folderlabel': label})
+            {'docno': file, 'folder': folder, 'box': box, 'title': title, 'folderlabel': label})
 
     return trainingSet
 
