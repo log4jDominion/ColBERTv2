80743efc79ebe159d00d24e30ff8bef8db0e2486
log4jDominion <bhardwajshashank1506@gmail.com>
2025-01-13 16:46:53 -0500

    Update train_colbert.py

--- src/sushi/train_colbert.py
+++ src/sushi/train_colbert.py
@@ -38,6 +38,7 @@ def colbert_search(queryIdx, query):
 
     for passage_id, passage_rank, passage_score in zip(*results):
         ranked_list.append(labels[passage_id])
+        print(f'QueryIndex : {queryIdx} \t\t PassageId and PassageScore: {[passage_id, passage_score]}')
         query_results.setdefault(queryIdx, []).append([passage_id, passage_score])
         # print(f"\t{labels[passage_id]} \t\t [{passage_rank}] \t\t {passage_score:.1f} \t\t {searcher.collection[passage_id]}")
 
